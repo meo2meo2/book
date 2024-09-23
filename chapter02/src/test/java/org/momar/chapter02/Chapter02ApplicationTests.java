fb83c7b257a5a550ae013d697e90b0dc1fd4506b
koalameedo <meo2@njit.edu>
2024-09-22 22:17:46 -0700

    Adding application Context via XML

---
+++ chapter02/src/test/java/org/momar/chapter02/Chapter02ApplicationTests.java
@@ -0,0 +1,42 @@
+package org.momar.chapter02;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.testng.annotations.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
+import static org.testng.Assert.assertEquals;
+
+
+public class Chapter02ApplicationTests {
+
+    @Test
+    public void testHelloWorld() {
+        Greeter greeter = new HelloWorldGreeter();
+        final ByteArrayOutputStream out = new ByteArrayOutputStream();
+        try(PrintStream ps = new PrintStream(out, true,StandardCharsets.UTF_8)) {
+            greeter.setPrintStream(ps);
+            greeter.greet();
+
+        }
+        String data = out.toString(StandardCharsets.UTF_8);
+        assertEquals(data,"Hello World");
+
+    }
+
+    @Test
+    public void testHelloWorldWithApplicationContext()
+    {
+
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("/applicationContext.xml");
+        Greeter greeter = (Greeter) ctx.getBean("helloGreeter");
+        ByteArrayOutputStream out = ctx.getBean("baos",ByteArrayOutputStream.class);
+        greeter.greet();
+        String data = out.toString(StandardCharsets.UTF_8);
+        assertEquals(data,"Hello World");
+    }
+
+}
